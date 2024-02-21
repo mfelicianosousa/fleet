5d167d00d4b272a8e9af68d12763b4de172a372c
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-02-21 17:34:46 -0400

    Create templates

--- src/main/java/br/com/mfsdevsystem/fleet/ApplicationController.java
+++ src/main/java/br/com/mfsdevsystem/fleet/ApplicationController.java
@@ -11,8 +11,33 @@ public class ApplicationController {
 		return "index";
 	}
 	
-	@GetMapping("/widgets")
-	public String widgets() {
-		return "widgets";
+	@GetMapping("/rh")
+	public String rh() {
+		return "/rh/index";
 	}
+	
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "/fleet/index";
+	}
+	
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "/hel/index";
+	}
+	
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "/accounts/index";
+	}
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "/payroll/index";
+	}
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "/parameters/index";
+	}
+	
+	
 }
